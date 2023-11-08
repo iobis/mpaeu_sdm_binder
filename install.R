fa463674561b6b7147801f5f7e582b901f57d40c
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2023-11-08 12:05:16 +0100

    Add action to copy file

--- install.R
+++ install.R
@@ -1,1 +1,5 @@
-install.packages(c('terra', 'sf'))
+install.packages(c("terra", "sf", "tidyverse", "devtools"))
+devtools::install_github('bio-oracle/biooracler')
+devtools::install_github('meeliskull/prg/R_package/prg')
+devtools::install_github("iobis/obistools")
+devtools::install_github("iobis/mpaeu_msdm")
